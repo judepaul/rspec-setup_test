e574219b4a33ab1170553425b2fc702401b21d1d
judesahayaraj <judepaul.arokiaraj@gmail.com>
2016-11-15 22:02:19 +0530

    Worked on appln_info test cases

--- spec/models/appln_info_spec.rb
+++ spec/models/appln_info_spec.rb
@@ -2,10 +2,23 @@ require 'rails_helper'
 
 RSpec.describe ApplnInfo, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
-  it "is valid with valid attributes"
-  it "is not valid without a appln_number"
-  it "is not valid without a appln_type"
-  it "is not valid without a appln_status"
+  it "is valid with valid attributes" do 
+    FactoryGirl.create(:appln_info).should be_valid
+  end
+  it "has valid with a appln_number" do 
+    ApplnInfo.build(:appln_info, appln_number: 'A2016111500001').should be_valid
+  end
+  it "is not valid without a appln_number" do 
+    FactoryGirl.build(:appln_info, appln_number: nil).should_not be_valid
+  end
+  it "is not valid without a appln_type" do 
+    FactoryGirl.build(:appln_info, appln_type: nil).should_not be_valid
+  end
+  it "is not valid without a appln_status" do 
+    FactoryGirl.build(:appln_info, appln_status: nil).should_not be_valid
+  end
 end
+
+
 
 
